aa27284b5c461eb4a6539ced634768ee1a4b2a72
yinwx0 <3314572822@qq.com>
2024-10-08 09:41:53 +0800

    第一次前端工程化

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+    title: 'Vue 3 + TypeScript 学习文档',
+    description: "详细学习 Vue 3 和 TypeScript 的指南",
+    themeConfig: {
+        siteTitle: "前端学习",
+        logo: "/assets/logo.png",
+        nav: [
+            { text:"首页", link:"/"},
+            { text:"springboot", link:"/springboot/"},
+            { text:"vue3", link:"/vue3/"},
+            { text:"API 参考", link:"/api/"},
+            { text:"常见问题", link:"/faq/"},
+        ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+        ],
+        sidebar: {
+            '/springboot/': [
+                {
+                    text: 'SpringBoot',
+                    collapsible: true,
+                    items: [
+                        { text: '介绍', link: '/springboot/' },
+                        { text: '安装', link: '/springboot/installation' },
+                        { text: '基本概念', link: '/springboot/concepts' },
+                    ],
+                },
+            ],
+            '/vue3/': [
+                {
+                    text: 'Vue3',
+                    collapsible: true,
+                    items: [
+                        { text: '介绍', link: '/vue3/' },
+                        { text: '安装', link: '/vue3/installation' },
+                        { text: '基本概念', link: '/vue3/concepts' },
+                    ],
+                },
+            ],
+        },
+        footer: {
+            message: "用心学习 Vue3 和 TypeScript",
+            copyright: 'Cppyright © 2024 wxyin',
+        },
+    },
+});
---
+++ docs/index.md
@@ -0,0 +1,33 @@
+---
+layout: home
+
+hero:
+  name: "前端工程化"
+  text: "Vue 3 + Vite2 + TypeScript 学习文档"
+  tagline: "学海无涯苦作舟"
+  image:
+    src: "/assets/logo.png"
+    alt: 前端工程化
+  actions:
+    - theme: brand
+      text: Spring boot
+      link: /springboot/
+    - theme: alt
+      text: Vue3
+      link: /vue3/
+
+features:
+  - icon: 🔥
+    title: 轻松入门
+    details: 从头开始，循序渐进地学习 Vue3 和 TypeScript。
+  - icon: 💻
+    title: 最佳实践
+    details: 涵盖从组件设计到代码优化的实际开发经验。
+  - icon: 🚀
+    title: 高效开发
+    details: 通过 VitePress 和 TypeScript ，快速搭建高效，可维护的 Vue3 应用。
+---
+
+<div style="text-align: center; margin-top: 50px;">
+    <em> ✨ Happy Coding! ✨</em>
+</div>
